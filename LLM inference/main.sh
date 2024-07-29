58f244c1a4022e267822a024dcb531e4be4cae01
Patrik <patrik.valabek@stuba.sk>
2024-07-29 11:19:24 +0200

    mistral test

--- LLM inference/main.sh
+++ LLM inference/main.sh
@@ -1,3 +1,3 @@
 module load singularity
-time singularity exec --nv /storage-data/singularity_containers/pt-2.3_llm.sif python3 aya_pred.py
+time singularity exec --nv /storage-data/singularity_containers/pt-2.3_llm.sif python3 mistral_pred.py
 
---
+++ LLM inference/mistral_pred.py
@@ -0,0 +1,41 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import pandas as pd
+import torch
+import numpy as np
+import random
+
+seed = 42
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+
+# load only the test data
+splits = {'train': 'data/train-00000-of-00001.parquet', 'test': 'data/test-00000-of-00001.parquet', 'validation': 'data/validation-00000-of-00001.parquet'}
+df = pd.read_parquet("hf://datasets/openlifescienceai/medmcqa/" + splits["validation"])
+df = df[df['choice_type'] != 'multi'] # only single choice questions, multi choice questions are not properly defined by the dataset
+
+# create prompts for the model
+df['prompt'] = None
+df['answer'] = None
+for index, row in df.iterrows():
+    df.at[index, 'prompt'] = row['question'] + '\nchoose only one of the following options: \na. ' + row['opa'] + '\nb. ' + row['opb'] + '\nc. ' + row['opc'] + '\nd. ' + row['opd']
+    
+df = df.sample(n=500, random_state=seed) # sample only 500 questions for testing
+
+b = 0
+for index, row in df.iterrows():
+    #print(row['prompt'])
+    prompt_inputs = tokenizer.encode(row['prompt'], return_tensors="pt")
+    prompt_outputs = model.generate(prompt_inputs, max_new_tokens=7)
+    df.loc[index, 'answer'] = tokenizer.decode(prompt_outputs[0], skip_special_tokens=True)
+    print(b)
+    b = b + 1
+
+print(df.head())
+
+filename = "data/test_mistral"
+df.to_csv(filename+".csv", index=False)
