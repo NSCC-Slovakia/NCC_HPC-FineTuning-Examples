eac1e476a17dc547e9d03a7dd621a0d0d7eb2eb9
Patrik <patrik.valabek@stuba.sk>
2024-07-30 14:03:59 +0200

    cuda

--- LLM inference/mistral_pred.py
+++ LLM inference/mistral_pred.py
@@ -38,7 +38,7 @@ df = df.sample(n=500, random_state=seed) # sample only 500 questions for testing
 b = 0
 for index, row in df.iterrows():
     #print(row['prompt'])
-    prompt_inputs = tokenizer.encode(row['prompt'], return_tensors="pt")
+    prompt_inputs = tokenizer.encode(row['prompt'], return_tensors="pt").to("cuda")
     prompt_outputs = model.generate(prompt_inputs, max_new_tokens=7)
     df.loc[index, 'answer'] = tokenizer.decode(prompt_outputs[0], skip_special_tokens=True)
     print(tokenizer.decode(prompt_outputs[0], skip_special_tokens=True))
