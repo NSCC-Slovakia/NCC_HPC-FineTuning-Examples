9af06147166a9b28addaded1ccb0bd4cdfaacca6
Patrik <patrik.valabek@stuba.sk>
2024-08-16 13:41:01 +0200

    Added run scripts

--- LLM inference/main.sh
+++
@@ -1,3 +0,0 @@
-module load singularity
-time singularity exec --nv /storage-data/singularity_containers/pt-2.3_llm.sif python3 aya_pred.py
-
--- LLM training/main.sh
+++
@@ -1,2 +0,0 @@
-module load singularity
-time singularity exec --nv /storage-data/singularity_containers/pt-2.3_llm.sif python3 mistral_trained_pred.py
